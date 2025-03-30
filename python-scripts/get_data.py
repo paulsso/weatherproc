d02d61a7eb96c45fa4e5a98f12c82c1ed6f1b355
Oskar Paulsson <oskar@paulsson.dev>
2025-03-30 19:38:36 +0200

    update

--- python-scripts/get_data.py
+++ python-scripts/get_data.py
@@ -10,14 +10,14 @@ def get_weather_data() -> bytes:
     station = os.getenv("SMHI_STATION")
     period = os.getenv("SMHI_PERIOD")
     
-    endpoint = f"/parameter/{parameter}/station/{station}/period/{period}/data.csv"
+    endpoint = f"parameter/{parameter}/station/{station}/period/{period}/data.csv"
     headers = {
         "User-Agent": "curl/7.19.7 (x86_64-redhat-linux-gnu) libcurl/7.19.7 NSS/3.15.3 zlib/1.2.3 libidn/1.18 libssh2/1.4.2",
         "Accept": "*/*",
         "Accept-Encoding": "gzip, deflate"
     }
     
-    response = requests.get(f"{base_url}{endpoint}", headers=headers)
+    response = requests.get(f"{base_url}/{endpoint}", headers=headers)
     assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
     assert isinstance(response.content, bytes)
     return response.content
